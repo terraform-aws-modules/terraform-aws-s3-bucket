96140dd9ac28e67f2c26d0cf8936b37e0ed8041d
Anton Babenko <anton@antonbabenko.com>
2019-11-21 14:08:01 +0100

    Output bucket id from s3_bucket_policy to make sure that policy is present before it can be used

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "this_s3_bucket_id" {
   description = "The name of the bucket."
-  value       = element(concat(aws_s3_bucket.this.*.id, list("")), 0)
+  value       = element(concat(aws_s3_bucket_policy.this.*.id, aws_s3_bucket.this.*.id, list("")), 0)
 }
 
 output "this_s3_bucket_arn" {
