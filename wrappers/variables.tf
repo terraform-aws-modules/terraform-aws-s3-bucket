217f286cca6b46f12abadac2c015edbbb451de29
Anton Babenko <anton@antonbabenko.com>
2021-05-15 19:18:37 +0200

    Fixed variable type

--- wrappers/variables.tf
+++ wrappers/variables.tf
@@ -1,5 +1,5 @@
 variable "items" {
   description = "Maps of items to create a wrapper from. Values are passed through to the module."
-  type        = map(any)
+  type        = any
   default     = {}
 }
