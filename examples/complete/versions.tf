011384d10ef742bad6f84dd57e4d886b0336db72
Alex Lundberg <34074700+lundbird@users.noreply.github.com>
2021-10-29 13:50:52 -0500

    feat: Replace hardcoded cloudfront canonical user ID in example (#113)

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -1,8 +1,8 @@
 terraform {
-  required_version = ">= 0.12.31"
+  required_version = ">= 0.13.1"
 
   required_providers {
-    aws    = ">= 3.50"
+    aws    = ">= 3.60"
     random = ">= 2.0"
   }
 }
