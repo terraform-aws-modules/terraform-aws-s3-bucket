306867549a547d7d00679c3f3c43a35b810de7e5
Max Tsybanov <maxtsybanov@insurify.com>
2021-12-22 15:24:49 -0500

    Update object_ownership variable description Add `BucketOwnerEnforced` value with a description to available options.

--- variables.tf
+++ variables.tf
@@ -167,7 +167,7 @@ variable "control_object_ownership" {
 }
 
 variable "object_ownership" {
-  description = "Object ownership. Valid values: BucketOwnerPreferred or ObjectWriter. 'BucketOwnerPreferred': Objects uploaded to the bucket change ownership to the bucket owner if the objects are uploaded with the bucket-owner-full-control canned ACL. 'ObjectWriter': The uploading account will own the object if the object is uploaded with the bucket-owner-full-control canned ACL."
+  description = "Object ownership. Valid values: BucketOwnerEnforced, BucketOwnerPreferred or ObjectWriter. 'BucketOwnerEnforced': ACLs are disabled, and the bucket owner automatically owns and has full control over every object in the bucket. 'BucketOwnerPreferred': Objects uploaded to the bucket change ownership to the bucket owner if the objects are uploaded with the bucket-owner-full-control canned ACL. 'ObjectWriter': The uploading account will own the object if the object is uploaded with the bucket-owner-full-control canned ACL."
   type        = string
   default     = "ObjectWriter"
 }
