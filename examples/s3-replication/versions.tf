ce9e719082c42db0560ad77a703cee5ee780e9a4
Max Tsybanov <5286992+tsybanov@users.noreply.github.com>
2022-01-12 03:43:27 -0500

    feat: Update object_ownership variable description (#121)

--- examples/s3-replication/versions.tf
+++ examples/s3-replication/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.13.1"
 
   required_providers {
-    aws    = ">= 3.64"
+    aws    = ">= 3.69"
     random = ">= 2.0"
   }
 }
--- versions.tf
+++ versions.tf
@@ -2,6 +2,6 @@ terraform {
   required_version = ">= 0.13.1"
 
   required_providers {
-    aws = ">= 3.64"
+    aws = ">= 3.69"
   }
 }
