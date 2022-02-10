e9f0fcc5aaa90d19fd93800fc9bb99b270546f13
Orest Kapko <kapko2311@gmail.com>
2022-02-10 16:13:48 +0200

    feat: Added source_account for lambda_permission resource (#135)

--- modules/notification/main.tf
+++ modules/notification/main.tf
@@ -64,6 +64,7 @@ resource "aws_lambda_permission" "allow" {
   qualifier           = lookup(each.value, "qualifier", null)
   principal           = "s3.amazonaws.com"
   source_arn          = local.bucket_arn
+  source_account      = lookup(each.value, "source_account", null)
 }
 
 # SQS Queue
