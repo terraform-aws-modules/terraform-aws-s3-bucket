4d72cdb6802efec9ce1f75ce0f3366e1117131ab
Anton Babenko <anton@antonbabenko.com>
2022-03-29 21:37:04 +0200

    Upgraded wrappers

--- wrappers/notification/main.tf
+++ wrappers/notification/main.tf
@@ -3,12 +3,13 @@ module "wrapper" {
 
   for_each = var.items
 
-  create               = lookup(each.value, "create", true)
-  create_sns_policy    = lookup(each.value, "create_sns_policy", true)
-  create_sqs_policy    = lookup(each.value, "create_sqs_policy", true)
-  bucket               = lookup(each.value, "bucket", "")
-  bucket_arn           = lookup(each.value, "bucket_arn", null)
-  lambda_notifications = lookup(each.value, "lambda_notifications", {})
-  sqs_notifications    = lookup(each.value, "sqs_notifications", {})
-  sns_notifications    = lookup(each.value, "sns_notifications", {})
+  create               = try(each.value.create, true)
+  create_sns_policy    = try(each.value.create_sns_policy, true)
+  create_sqs_policy    = try(each.value.create_sqs_policy, true)
+  bucket               = try(each.value.bucket, "")
+  bucket_arn           = try(each.value.bucket_arn, null)
+  eventbridge          = try(each.value.eventbridge, null)
+  lambda_notifications = try(each.value.lambda_notifications, {})
+  sqs_notifications    = try(each.value.sqs_notifications, {})
+  sns_notifications    = try(each.value.sns_notifications, {})
 }
