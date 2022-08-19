f9f2b772d2b8526ae20e474a55ccb7bee951099d
Alexandre Chanudet <achanudet@nottle.fr>
2022-08-19 20:25:34 +0200

    chore: add source_hash parameter

--- wrappers/object/main.tf
+++ wrappers/object/main.tf
@@ -27,4 +27,5 @@ module "wrapper" {
   object_lock_legal_hold_status = try(each.value.object_lock_legal_hold_status, var.defaults.object_lock_legal_hold_status, null)
   object_lock_mode              = try(each.value.object_lock_mode, var.defaults.object_lock_mode, null)
   object_lock_retain_until_date = try(each.value.object_lock_retain_until_date, var.defaults.object_lock_retain_until_date, null)
+  source_hash                   = try(each.value.source_hash, var.defaults.source_hash, null)
 }
