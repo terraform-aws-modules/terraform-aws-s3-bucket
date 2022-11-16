f3e396a3502cbd10c0c51005361ec7ae0d4dd58c
xdays <easedays@gmail.com>
2022-11-16 22:33:39 +0800

    enable jsonencode support for replication_configuration

--- main.tf
+++ main.tf
@@ -8,11 +8,12 @@ locals {
   attach_policy = var.attach_require_latest_tls_policy || var.attach_elb_log_delivery_policy || var.attach_lb_log_delivery_policy || var.attach_deny_insecure_transport_policy || var.attach_inventory_destination_policy || var.attach_policy
 
   # Variables with type `any` should be jsonencode()'d when value is coming from Terragrunt
-  grants               = try(jsondecode(var.grant), var.grant)
-  cors_rules           = try(jsondecode(var.cors_rule), var.cors_rule)
-  lifecycle_rules      = try(jsondecode(var.lifecycle_rule), var.lifecycle_rule)
-  intelligent_tiering  = try(jsondecode(var.intelligent_tiering), var.intelligent_tiering)
-  metric_configuration = try(jsondecode(var.metric_configuration), var.metric_configuration)
+  grants                    = try(jsondecode(var.grant), var.grant)
+  cors_rules                = try(jsondecode(var.cors_rule), var.cors_rule)
+  lifecycle_rules           = try(jsondecode(var.lifecycle_rule), var.lifecycle_rule)
+  intelligent_tiering       = try(jsondecode(var.intelligent_tiering), var.intelligent_tiering)
+  metric_configuration      = try(jsondecode(var.metric_configuration), var.metric_configuration)
+  replication_configuration = try(jsondecode(var.replication_configuration), var.replication_configuration)
 }
 
 resource "aws_s3_bucket" "this" {
