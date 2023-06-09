8aa5a758ac74f7d4d5fbb3fae3373a7cbef78da1
Chris Fortier <cfortier2@gmail.com>
2023-06-09 14:51:00 -0400

    fixes attach_deny_insecure_transport_policy
    When attempting to apply `attach_deny_insecure_transport_policy` on some buckets the module wants to recreate the bucket due to the owner policy even when it's set correctly. This PR fixes that. 
    
    Error message:
    ```  # module.${bucket}.module.s3_bucket.aws_s3_bucket_ownership_controls.this[0] must be replaced
    -/+ resource "aws_s3_bucket_ownership_controls" "this" {
          ~ bucket = "${bucket}" # forces replacement -> (known after apply) # forces replacement
          ~ id     = "${bucket}" -> (known after apply)```

--- main.tf
+++ main.tf
@@ -808,7 +808,7 @@ resource "aws_s3_bucket_public_access_block" "this" {
 resource "aws_s3_bucket_ownership_controls" "this" {
   count = local.create_bucket && var.control_object_ownership ? 1 : 0
 
-  bucket = local.attach_policy ? aws_s3_bucket_policy.this[0].id : aws_s3_bucket.this[0].id
+  bucket = aws_s3_bucket.this[0].id
 
   rule {
     object_ownership = var.object_ownership
