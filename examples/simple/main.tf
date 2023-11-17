30b70f7202327b7140a0353ff146ea5573729605
Brittan DeYoung <32572259+brittandeyoung@users.noreply.github.com>
2023-11-17 12:12:52 -0500

    examples: Add simple example with known bucket name

---
+++ examples/simple/main.tf
@@ -0,0 +1,14 @@
+locals {
+    bucket_name = "known-name"
+}
+provider "aws" {
+  region      = "eu-west-1"
+}
+
+module "simple_bucket" {
+  source = "../../"
+
+  bucket = local.bucket_name
+
+  force_destroy = true
+}
---
+++ examples/simple/versions.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">= 1.6.4"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.9"
+    }
+  }
+}
