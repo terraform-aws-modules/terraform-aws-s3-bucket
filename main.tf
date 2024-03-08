58b15a16a3b43d49c3097efe78fe323349d5e36b
apmarshall <apmarshall@soren.tech>
2024-03-07 22:45:51 -0500

    Use nisst_mode to control public access

--- main.tf
+++ main.tf
@@ -914,10 +914,10 @@ resource "aws_s3_bucket_public_access_block" "this" {
 
   bucket = aws_s3_bucket.this[0].id
 
-  block_public_acls       = var.block_public_acls
-  block_public_policy     = var.block_public_policy
-  ignore_public_acls      = var.ignore_public_acls
-  restrict_public_buckets = var.restrict_public_buckets
+  block_public_acls       = var.nist_mode ? true : var.block_public_acls
+  block_public_policy     = var.nist_mode ? true : var.block_public_policy
+  ignore_public_acls      = var.nist_mode ? true : var.ignore_public_acls
+  restrict_public_buckets = var.nist_mode ? true : var.restrict_public_buckets
 }
 
 resource "aws_s3_bucket_ownership_controls" "this" {
