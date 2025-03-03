808455167df46d6a93e30a19e74f69b1cd4ef5b1
Ram0469 <abhirampaturu@gmail.com>
2025-03-02 21:34:14 -0500

    Update main.tf

--- examples/account-public-access/main.tf
+++ examples/account-public-access/main.tf
@@ -1,14 +1,10 @@
 provider "aws" {
-  region = local.region
+  region = var.region
 
   # Make it faster by skipping something
   skip_metadata_api_check     = true
   skip_region_validation      = true
   skip_credentials_validation = true
-}
-
-locals {
-  region = "eu-west-1"
 }
 
 module "account_public_access" {
