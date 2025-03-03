f6484f5dc0a9379836a047c25dabaa49c8839f2e
Ram0469 <abhirampaturu@gmail.com>
2025-03-02 21:35:21 -0500

    Update main.tf

--- examples/account-public-access/main.tf
+++ examples/account-public-access/main.tf
@@ -13,5 +13,5 @@ module "account_public_access" {
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
-  restrict_public_buckets = true
+  restrict_public_buckets = var.restrict_public_buckets
 }
