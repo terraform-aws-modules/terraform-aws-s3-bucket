541232b78c11a51f150d49a9a8167c8b828aa4b5
Melissa Greenbaum <69476188+magreenbaum@users.noreply.github.com>
2025-05-16 16:34:40 -0400

    feat: S3 table and table bucket CMK encryption support (#327)

--- wrappers/table-bucket/main.tf
+++ wrappers/table-bucket/main.tf
@@ -5,7 +5,8 @@ module "wrapper" {
 
   create                                 = try(each.value.create, var.defaults.create, true)
   create_table_bucket_policy             = try(each.value.create_table_bucket_policy, var.defaults.create_table_bucket_policy, false)
-  maintenance_configuration              = try(each.value.maintenance_configuration, var.defaults.maintenance_configuration, {})
+  encryption_configuration               = try(each.value.encryption_configuration, var.defaults.encryption_configuration, null)
+  maintenance_configuration              = try(each.value.maintenance_configuration, var.defaults.maintenance_configuration, null)
   table_bucket_name                      = try(each.value.table_bucket_name, var.defaults.table_bucket_name, null)
   table_bucket_override_policy_documents = try(each.value.table_bucket_override_policy_documents, var.defaults.table_bucket_override_policy_documents, [])
   table_bucket_policy                    = try(each.value.table_bucket_policy, var.defaults.table_bucket_policy, null)
