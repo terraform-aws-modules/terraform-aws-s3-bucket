8833269e206a62030a259032cf039db5333e8160
Tiago Vieira <80104680+tiagovmvieira@users.noreply.github.com>
2025-08-25 16:44:30 +0100

    feat: Add skip_destroy argument in the aws_s3_bucket_public_access_bl… (#352)

--- main.tf
+++ main.tf
@@ -1148,6 +1148,7 @@ resource "aws_s3_bucket_public_access_block" "this" {
   block_public_policy     = var.block_public_policy
   ignore_public_acls      = var.ignore_public_acls
   restrict_public_buckets = var.restrict_public_buckets
+  skip_destroy            = var.skip_destroy_public_access_block
 }
 
 resource "aws_s3_bucket_ownership_controls" "this" {
